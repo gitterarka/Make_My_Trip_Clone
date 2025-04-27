1cdc3ab9d060a6738256dd1855bd3deabed66270
gitterarka <94968079+gitterarka@users.noreply.github.com>
2025-04-27 14:04:14 +0530

    Update flight.js
    flight.js controller added

--- api/controllers/flight.js
+++ api/controllers/flight.js
@@ -1,81 +1,101 @@
-// import Room from "../models/Room.js";
-// import { createError } from "../utils/error.js";
+import Flight from "../models/Flight.js";
 
-// export const createRoom = async (req, res, next) => {
-//   const hotelId = req.params.hotelid;
-//   const newRoom = new Room(req.body);
+// Create a new flight
+export const createFlight = async (req, res, next) => {
+  const newFlight = new Flight(req.body);
+  try {
+    const savedFlight = await newFlight.save();
+    res.status(200).json(savedFlight);
+  } catch (err) {
+    next(err);
+  }
+};
 
-//   try {
-//     const savedRoom = await newRoom.save();
-//     try {
-//       await Hotel.findByIdAndUpdate(hotelId, {
-//         $push: { rooms: savedRoom._id },
-//       });
-//     } catch (err) {
-//       next(err);
-//     }
-//     res.status(200).json(savedRoom);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+// Update an existing flight
+export const updateFlight = async (req, res, next) => {
+  try {
+    const updatedFlight = await Flight.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedFlight);
+  } catch (err) {
+    next(err);
+  }
+};
 
-// export const updateRoom = async (req, res, next) => {
-//   try {
-//     const updatedRoom = await Room.findByIdAndUpdate(
-//       req.params.id,
-//       { $set: req.body },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedRoom);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// export const updateRoomAvailability = async (req, res, next) => {
-//   try {
-//     await Room.updateOne(
-//       { "roomNumbers._id": req.params.id },
-//       {
-//         $push: {
-//           "roomNumbers.$.unavailableDates": req.body.dates
-//         },
-//       }
-//     );
-//     res.status(200).json("Room status has been updated.");
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// export const deleteRoom = async (req, res, next) => {
-//   const hotelId = req.params.hotelid;
-//   try {
-//     await Room.findByIdAndDelete(req.params.id);
-//     try {
-//       await Hotel.findByIdAndUpdate(hotelId, {
-//         $pull: { rooms: req.params.id },
-//       });
-//     } catch (err) {
-//       next(err);
-//     }
-//     res.status(200).json("Room has been deleted.");
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// export const getRoom = async (req, res, next) => {
-//   try {
-//     const room = await Room.findById(req.params.id);
-//     res.status(200).json(room);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// export const getRooms = async (req, res, next) => {
-//   try {
-//     const rooms = await Room.find();
-//     res.status(200).json(rooms);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+// Delete a flight
+export const deleteFlight = async (req, res, next) => {
+  try {
+    await Flight.findByIdAndDelete(req.params.id);
+    res.status(200).json("Flight has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get a specific flight
+export const getFlight = async (req, res, next) => {
+  try {
+    const flight = await Flight.findById(req.params.id);
+    res.status(200).json(flight);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getFlights = async (req, res, next) => {
+    const { minPrice = 1, maxPrice = 100000, limit, ...others } = req.query;
+    try {
+      const flights = await Flight.find({
+        ...others,
+        price: {
+          $gte: Number(minPrice),
+          $lte: Number(maxPrice),
+        },
+      }).limit(Number(limit));
+      res.status(200).json(flights);
+    } catch (err) {
+      next(err);
+    }
+  };
+  export const countByPlaces = async (req, res, next) => {
+    const destinations = req.query.destinations.split(",");
+    try {
+      const list = await Promise.all(
+        destinations.map((place) => {
+          return Flight.countDocuments({ to: place });
+        })
+      );
+      res.status(200).json(list);
+    } catch (err) {
+      next(err);
+    }
+  };
+  export const countByAirline = async (req, res, next) => {
+    const airlines = req.query.airlines.split(",");
+    try {
+      const list = await Promise.all(
+        airlines.map((airline) => {
+          return Flight.countDocuments({ airline: airline });
+        })
+      );
+      res.status(200).json(list);
+    } catch (err) {
+      next(err);
+    }
+  };
+  export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(",");
+    try {
+      const list = await Promise.all(
+        cities.map((city) => {
+          return Flight.countDocuments({ from: city });
+        })
+      );
+      res.status(200).json(list);
+    } catch (err) {
+      next(err);
+    }
+  };

bda7dfe5359334c11b053f3330c078e032698d02
gitterarka <94968079+gitterarka@users.noreply.github.com>
2025-04-27 13:59:29 +0530

    Update datatablesource.js
    Flight Datatable created

--- admin/src/datatablesource.js
+++ admin/src/datatablesource.js
@@ -83,3 +83,63 @@ export const roomColumns = [
     width: 100,
   },
 ];
+export const flightColumns = [
+    { field: "_id", headerName: "ID", width: 250 },
+    {
+      field: "flightNumber",
+      headerName: "Flight Number",
+      width: 150,
+    },
+    {
+      field: "airline",
+      headerName: "Airline",
+      width: 180,
+    },
+    {
+      field: "from",
+      headerName: "From",
+      width: 150,
+    },
+    {
+      field: "to",
+      headerName: "To",
+      width: 150,
+    },
+    {
+      field: "departureTime",
+      headerName: "Departure Time",
+      width: 180,
+      type: "dateTime",
+    },
+    {
+      field: "arrivalTime",
+      headerName: "Arrival Time",
+      width: 180,
+      type: "dateTime",
+    },
+    {
+      field: "duration",
+      headerName: "Duration",
+      width: 120,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 120,
+    },
+    {
+      field: "seatsAvailable",
+      headerName: "Seats Available",
+      width: 160,
+    },
+    {
+      field: "seatType",
+      headerName: "Seat Type",
+      width: 130,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 150,
+    },
+];

4dc2e0f4302c944b809db778cfaaba315e3882ad
gitterarka <94968079+gitterarka@users.noreply.github.com>
2025-04-28 21:37:52 +0530

    Update useFetch.js

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null); // or [] if always array
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,9 +14,8 @@ const useFetch = (url) => {
         setData(res.data);
       } catch (err) {
         setError(err);
-      } finally {
-        setLoading(false);
       }
+      setLoading(false);
     };
     fetchData();
   }, [url]);
@@ -28,9 +27,8 @@ const useFetch = (url) => {
       setData(res.data);
     } catch (err) {
       setError(err);
-    } finally {
-      setLoading(false);
     }
+    setLoading(false);
   };
 
   return { data, loading, error, reFetch };
